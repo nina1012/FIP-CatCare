3ad59f4e452fc32fc44c49baa3618ebeb3f1c1d6
Nina <labferment10@gmail.com>
2024-08-09 20:29:13 +0200

    added api-client.ts into lib folder

---
+++ src/lib/api-client.ts
@@ -0,0 +1,26 @@
+import Axios, { InternalAxiosRequestConfig } from 'axios';
+
+import { env } from '@/config/env';
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = 'application/json';
+  }
+  return config;
+}
+
+export const apiClient = Axios.create({
+  baseURL: env.BASE_API_URL,
+});
+
+apiClient.interceptors.request.use(authRequestInterceptor);
+apiClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    console.error(message);
+    return Promise.reject(error);
+  },
+);

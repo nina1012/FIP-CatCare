dcbc66b388d185efba8f779ce79afc9878bbc5f7
Nina <labferment10@gmail.com>
2025-01-25 14:09:15 +0100

    fixed bugs in daily-log-form.tsx

--- src/features/daily-logs/components/daily-log-form.tsx
+++ src/features/daily-logs/components/daily-log-form.tsx
@@ -22,7 +22,7 @@ import { useUpdateDailyLog } from '../api/update-daily-log';
 import { DailyLog } from '../types';
 
 const dailyLogsSchema = z.object({
-  day: z.string().optional().default('0'),
+  day: z.string().optional().default(''),
   dose: z.string(),
   weight: z.string().optional(),
   medication_name: z.enum(['GS-15', 'GS-20', 'GS-30']),
@@ -37,10 +37,7 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
   const { catID } = useParams();
   const { toast } = useToast();
 
-  const { updateDailyLog, isPending: isUpdating } = useUpdateDailyLog(
-    logID as string,
-  );
-  console.log(isUpdating);
+  const { updateDailyLog, isPending: isUpdating } = useUpdateDailyLog(logID!);
   const { createDailyLog, isCreatingDailyLog: isCreating } = useCreateDailyLog({
     onSuccess: () => {
       toast({
@@ -57,27 +54,21 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
     },
   });
 
-  const { dailyLog } = useDailyLog(logID as string);
+  const { dailyLog, isLoading } = useDailyLog(logID!);
 
-  if (!logID || !dailyLog) return;
-
-  const handleSubmit = (formValues: Partial<DailyLog>) => {
-    const logData = {
-      ...dailyLog[0],
-      ...formValues,
-      cat_id: catID,
-      log_date: !logID ? new Date().toISOString() : dailyLog[0].log_date,
-      updated_at: logID ? new Date().toISOString() : dailyLog[0].updated_at,
-    };
-
+  const handleSubmit = (data: Partial<DailyLog>) => {
     if (logID) {
-      updateDailyLog(logData);
+      updateDailyLog({ log_id: logID, ...data, cat_id: catID });
     } else {
-      createDailyLog(logData);
+      createDailyLog({ cat_id: catID, ...data });
     }
   };
 
-  const { weight, medication_name, dose, note, day } = dailyLog[0];
+  if (logID && isLoading) {
+    return <Spinner size="lg" className="mx-auto" />;
+  }
+
+  const defaultValues = logID ? (dailyLog?.[0] as DailyLog) : ({} as DailyLog); // added defaultValues in order to get rid of the errors that used to happen when user want to create new daily log
 
   return (
     <div>
@@ -91,25 +82,25 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
         <div className="my-4 flex w-full flex-col gap-2 rounded-sm border border-[#dfb028] bg-[#dfb028]/40 p-2 py-4 text-xs">
           <FileWarning className="mx-auto" />
           <p>
-            You can only post a daily log once a day, if you try to add more
+            You can only post a daily log once a day. If you try to add more
             logs, the app will crash and you&apos;ll need to refresh the page!
           </p>
         </div>
       )}
       <Form
         schema={dailyLogsSchema}
-        onSubmit={(values: Partial<DailyLog>) => {
-          handleSubmit(values);
-        }}
+        onSubmit={(values: Partial<DailyLog>) => handleSubmit(values)}
       >
         {({ register, formState, watch }) => {
-          const selectedBrand = watch('medication_name');
+          const selectedBrand =
+            watch('medication_name') || defaultValues.medication_name;
+
           return (
             <>
               <Input
                 registration={register('day')}
                 type="text"
-                defaultValue={day}
+                defaultValue={defaultValues.day || ''}
                 placeholder="Day 0"
                 error={formState.errors['day']}
                 disabled
@@ -119,7 +110,7 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
                 registration={register('dose')}
                 type="text"
                 pattern="[0-9]+([\.,][0-9]+)?"
-                defaultValue={dose || ''}
+                defaultValue={defaultValues.dose || ''}
                 placeholder="Dose of GS in mg"
                 error={formState.errors['dose']}
               />
@@ -127,20 +118,16 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
               <Input
                 registration={register('weight')}
                 type="text"
-                defaultValue={weight || ''}
+                pattern="[0-9]+([\.,][0-9]+)?"
+                defaultValue={defaultValues.weight || ''}
                 placeholder="Weight in kg"
                 error={formState.errors['weight']}
-                pattern="[0-9]+([\.,][0-9]+)?"
                 step="0.01"
               />
 
               <CustomSelect registration={register('medication_name')}>
                 <SelectTrigger className="text-inherit">
-                  <SelectValue
-                    placeholder={
-                      selectedBrand || medication_name || 'Select brand'
-                    }
-                  />
+                  <SelectValue placeholder={selectedBrand || 'Select brand'} />
                 </SelectTrigger>
                 <SelectContent>
                   <SelectItem value="GS-15">GS-15</SelectItem>
@@ -152,13 +139,19 @@ export const DailyLogsForm = ({ logID }: DailyLogsFormProps) => {
               <Textarea
                 className="resize-none"
                 placeholder="Enter today's symptoms"
-                defaultValue={note || ''}
+                defaultValue={defaultValues.note || ''}
                 registration={register('note')}
               ></Textarea>
+
               <div>
-                <Button type="submit" className="w-full">
-                  {!isUpdating ||
-                    (isCreating && <Spinner size="sm" className="bg-white" />)}
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={isUpdating || isCreating}
+                >
+                  {(isUpdating || isCreating) && (
+                    <Spinner size="sm" className="bg-white" />
+                  )}
                   {logID ? 'Update daily log' : 'Add new daily log'}
                 </Button>
               </div>
